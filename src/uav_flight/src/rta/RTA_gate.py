c3fb60e8d61e9e852b685bcd92030ba60d0fa634
Bionic-sock <isaaciscool19@gmail.com>
2020-06-16 18:07:03 -0600

    adding RTA and fixing world

--- src/uav_flight/src/rta/RTA_gate.py
+++ src/uav_flight/src/rta/RTA_gate.py
@@ -1,8 +1,158 @@
+#! /usr/bin/python
+
+####################
+# Import libraries #
+####################
+
+import math
+import rospy
+import mavros
+import std_msgs
+import tf 
+import sys, select, os, tty, termios
+
+from geometry_msgs.msg import PoseStamped, TwistStamped
+from numpy import linspace, array
+from mavros_msgs.msg import GlobalPositionTarget, State
+from mavros_msgs.srv import CommandBool, SetMode, CommandTOL
+
+# Personal def
 from com_func import *
 from safe_cont import *
 from perform_cont import *
 
+###############
+# Definitions #
+###############
+
+# Actively watches for keypress #
+#################################
+def getch():
+
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+ 
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+# Defines the choices availible #
+#################################
+def menu():
+    print "1: Arm, Takeoff, and Loiter at altitude of 3m \
+     \n2: Land \n3: Kill node"
+
+# Arms quadrotor and commands it to takeoff and loiter at z = 3 #
+#################################################################
+def arm_takeoff():
+
+    rospy.wait_for_service('mavros/set_mode')
+    rospy.wait_for_service('mavros/cmd/arming')
+
+    pose_stamp = PoseStamped()
+
+    pose_stamp.pose.position.x = 0
+    pose_stamp.pose.position.y = 0
+    pose_stamp.pose.position.z = 3
+
+
+    for i in range(20):
+        setpoint_pub.publish(pose_stamp)
+        rate.sleep()
+
+    try:
+ 
+        last_request = rospy.Time.now()
+
+        offb_set_mode = flight_mode(custom_mode="OFFBOARD")
+        arm_client = arming_serv(True)
+        print "Take off and hover initiated"
+        char = 0
+
+        
+        while True:
+
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print "1: Start Mission\n2: Abort"
+
+            setpoint_pub.publish(pose_stamp)
+            rospy.sleep(.25)
+            char = getch()
+
+            if char == "1":
+                mission()
+                char = 0
+                break
+
+            if char == "2":
+                offb_set_mode = flight_mode(custom_mode="AUTO.LAND")
+                break
+
+    except rospy.ServiceException as e:
+        print("Failed to set OFFBOARD mode")
+
+# Generates waypoints for the quadrotor top follow #
+####################################################
+def mission():
+
+    pose_stamp = PoseStamped()
+
+    vehicle_xdot  = 0.2
+    vehicle_ydot  = 0.2
+    obstacle_list =  np.array([[1, 2], [3, 4], [5, 6]])
+    target        = np.array([x,y])
+    current_mode  = 0
+    safe_complete = 0
+
+    if abs(x) >= abs(y) and abs(x) >= abs(z):
+        gcoor = abs(x) 
+        x_lin = linspace(0, x, gcoor)
+        y_lin = linspace(0, y, gcoor)
+        z_lin = linspace(0, z, gcoor)
+
+    if abs(y) > abs(x) and abs(y) >= abs(z):
+        gcoor = abs(y)
+        x_lin = linspace(0, x, gcoor)
+        y_lin = linspace(0, y, gcoor)
+        z_lin = linspace(0, z, gcoor)
+
+    if abs(z) > abs(x) and abs(z) > abs(y):
+        gcoor = z
+        x_lin = linspace(0, x, gcoor)
+        y_lin = linspace(0, y, gcoor)
+        z_lin = linspace(0, z, gcoor)
+
+    cur_wpt = np.array([x_lin[0], y_lin[0]])
+
+    for i in range(gcoor):
+        print(x_lin[i])
+        print(y_lin[i])
+        print(z_lin[i])
+
+        wpt = RTA_Gate(x_lin[i], y_lin[i], vehicle_xdot, vehicle_ydot, obstacle_list, target, current_mode, safe_complete, cur_wpt)
+        
+        wpt_out = wpt[0]
+        print(wpt_out[0])
+        x_rta = wpt_out[0]
+        y_rta = wpt_out[1]
+
+        pose_stamp.pose.position.x = x_rta
+        pose_stamp.pose.position.y = y_rta
+        pose_stamp.pose.position.z = z_lin[i]
+
+        setpoint_pub.publish(pose_stamp)
+        rospy.sleep(.5)
+
+        if i == len(range(gcoor)): 
+            break
+
+# Responsible for switching between performance and safe controller #
+#####################################################################
 def RTA_Gate(vehicle_x, vehicle_y, vehicle_xdot, vehicle_ydot, obstacle_list, target, current_mode, safe_complete, cur_wpt):
+
         #Parameters:
         obs_to_consider = 2 #Number of obstacles to consider in avoidance
         tol = 2 #Time (seconds) tolerance to avoid obstacle (3 means should always have a 3 second buffer between vehicle and obstacle)
@@ -45,6 +195,8 @@ def RTA_Gate(vehicle_x, vehicle_y, vehicle_xdot, vehicle_ydot, obstacle_list, ta
             if violation == 0 and current_mode == 0:
                 wpt_performance = Performance_Controller(vehicle_x, vehicle_y, target)
                 #This would be reinforcement learning controller. We can replace with something else for now.
+                # wpt_out = wpt_performance # Assign wpt_ to a single variable that can be re-writen ########################### <--- Show Kendra
+                # print(wpt_out)
                 return wpt_performance, current_mode, safe_complete
 
             elif violation == 1 and current_mode == 0:
@@ -54,21 +206,87 @@ def RTA_Gate(vehicle_x, vehicle_y, vehicle_xdot, vehicle_ydot, obstacle_list, ta
                 start_safe = 1
                 wpt_safe_list = "global"
                 Safe_Controller_Init(vehicle_x, vehicle_y, vehicle_xdot, vehicle_ydot, target, obstacle_list[i])
+                print("middle")
                 return wpt_safe_list[0],  current_mode, safe_complete
 
             elif current_mode == 1 and safe_complete == 0:
                 #Continue to execute trajectory until completed
                 start_safe = 0
                 wpt_safe, safe_complete = Safe_Controller(vehicle_x, vehicle_y, target, cur_wpt)
-
+                # wpt_out = wpt_safe # Assign wpt_ to a single variable that can be re-writen
+                print("middle_bot")
                 return wpt_safe, current_mode, safe_complete
 
             else:
                 #Safe maneuver has been completed, hopefully we don't switch right back to violation so give buffer?
                 current_mode = 0
                 wpt_performance = Performance_Controller(vehicle_x, vehicle_y, target)
-
+                # wpt_out = wpt_performance # Assign wpt_ to a single variable that can be re-writen
+                print("def bottom")
                 return wpt_performance, current_mode, safe_complete
 
+# Allows for enough time to cleanly kill the node #
+###################################################
+def kill():
+    print "\n*Cough cough*"
+    rospy.sleep(.6)
+    print "\nI'm..."
+    rospy.sleep(.5)
+    print "\ndying"
+    rospy.sleep(.5)
+    rospy.signal_shutdown("Time to sleep!")
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+#########
+# Start #
+#########
+
 if __name__ == '__main__':
-    RTA_Gate()
+
+    global x, y, z, wpt_out
+
+    rospy.init_node('RTA_Gate', anonymous=False)
+
+    # Subscribers
+    state_sub      = rospy.Subscriber("mavros/state", State)
+
+    # Publishers
+    setpoint_pub   = rospy.Publisher("mavros/setpoint_position/local", PoseStamped, queue_size=10)
+    velocity_pub   = rospy.Publisher('mavros/setpoint_velocity/cmd_vel', TwistStamped, queue_size=10)
+
+    # Services
+    arming_serv    = rospy.ServiceProxy('mavros/cmd/arming', CommandBool)
+    flight_mode    = rospy.ServiceProxy('mavros/set_mode', SetMode)
+    takeoffService = rospy.ServiceProxy('mavros/cmd/takeoff', CommandTOL)
+
+    rate = rospy.Rate(20.0)
+
+    choice = '1'
+
+    while not rospy.is_shutdown() and choice in ['1', '2', '3']:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        menu()
+        choice = raw_input("Enter your imput: ");
+        x = 0
+        y = 0
+        z = 0
+
+        if choice == '1':
+            print "Enter destination coordinates: \n"
+            x = int(raw_input('x = '))
+            y = int(raw_input('y = '))
+            z = int(raw_input('z = '))       
+
+            if z <= 0:
+                print("z must be greater than 0\nAutomatically setting z = 1")
+                rospy.sleep(2)
+                z = 1
+
+            arm_takeoff()
+
+        if choice == '2':
+            offb_set_mode = flight_mode(custom_mode="AUTO.LAND")
+
+        if choice == '3':
+            kill()
+            break
